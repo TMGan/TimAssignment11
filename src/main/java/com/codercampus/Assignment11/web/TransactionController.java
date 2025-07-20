dadcb9c94419e46b185fed74d4caef9488c8e946
TMGan <gangadeentim@gmail.com>
2025-07-20 18:40:43 -0400

    1st submission

--- src/main/java/com/codercampus/Assignment11/web/TransactionController.java
+++ src/main/java/com/codercampus/Assignment11/web/TransactionController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
 import java.math.BigDecimal;
+import java.util.Comparator;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -21,11 +22,12 @@ public class TransactionController {
     @GetMapping("/transactions")
     public String getTransactions (ModelMap model) {
         List<Transaction> transactions = transactionService.findAll();
+        transactions.sort(Comparator.comparing(Transaction::getDate));
         model.put("transactions", transactions);
         return "transactions";
     }
     @GetMapping("/transactions/{transactionId}")
-    public String getTransaction (@PathVariable Integer transactionId, ModelMap model) {
+    public String getTransactionDetails (@PathVariable Integer transactionId, ModelMap model) {
         Transaction transaction = transactionService.findById(transactionId);
         model.put("transaction", transaction);
         return "transaction";
